0b511eb6b863441d7e83a9333c3eb3a74b3e2db9
ogatakun0113-dot <ogatakun0113@gmail.com>
2026-05-07 11:06:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 st.markdown('<p class="credit">開発/制作：緒方</p>', unsafe_allow_html=True)
-st.title('📱 伝送換算 (990h-FD0h)(HEX)')
+st.title('📱 伝送換算 (990-FD0)(HEX)')
 
 with st.expander("⚙️ 基本情報設定 (990h-FD0h基準)", expanded=True):
     col1, col2 = st.columns(2)
